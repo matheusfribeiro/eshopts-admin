24416217a870271bc99cad422f643cb9edb48374
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-12-12 19:05:01 -0300

    feat: orders entity (store)

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+
+
+export type OrderColumn = {
+  id: string
+  phone: string
+  address: string
+  isPaid: boolean
+  totalPrice: string
+  products: string
+  createdAt: string
+}
+
+export const columns: ColumnDef<OrderColumn>[] = [
+  {
+    accessorKey: "products",
+    header: "Produtos",
+  },
+  {
+    accessorKey: "phone",
+    header: "Fone",
+  },
+  {
+    accessorKey: "address",
+    header: "Endereço",
+  },
+  {
+    accessorKey: "totalPrice",
+    header: "Preço Final",
+  },
+  {
+    accessorKey: "isPaid",
+    header: "Pago",
+  },
+]
